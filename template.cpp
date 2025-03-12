896e9ace0f0f928c850c6dbb8ea6cddcf06d451d
Manthan Sharma <manthan2306s@gmail.com>
2025-03-12 17:23:21 +0530

    base1

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+void fastIO() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+}
+
+void solve() {}
+
+int main() {
+
+  int t;
+  cin >> t;
+
+  while (t--)
+    solve();
+
+  return 0;
+}
